17158f77150d0769814b3836cca9fb8e73468b70
Finn Baumann <hello@baumannfinn.de>
2022-11-08 10:00:25 +0100

    jar working from command line

--- src/main/java/TamarinAutoRunner/Main.java
+++ src/main/java/TamarinAutoRunner/Main.java
@@ -11,6 +11,7 @@ public class Main {
         String oracleFile = "";
         String tamarinBin = "";
 
+        // java -jar ./build/libs/TamarinAutoRunner-1.0-SNAPSHOT.jar ./exampleFiles/Netto.spthy -oracle=./exampleFiles/oracle.py -tamarin=/Users/finn/Documents/Research_Project_Tamarin/tamarin-prover/1.6.1/bin/tamarin-prover
         for (int i = 1; i < args.length; i++) {
             if (args[i].startsWith("-oracle")) {
                 oracleFile = args[i].split("=")[1];
