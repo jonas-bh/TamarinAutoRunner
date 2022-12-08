c96119608b84d1d0a92abccff62c2bfb8e2a26aa
Finn Baumann <hello@baumannfinn.de>
2022-12-08 11:21:49 +0100

    re-activated command line features

--- src/main/java/TamarinAutoRunner/Main.java
+++ src/main/java/TamarinAutoRunner/Main.java
@@ -11,24 +11,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // String protocol = args[0];
-        // String oracleFile = "";
-        // String tamarinBin = "";
-        String protocol = "/Users/finn/Documents/Research_Project_Tamarin/TamarinAutoRunner/exampleFiles/KC.spthy";
-        String oracleFile = "/Users/finn/Documents/Research_Project_Tamarin/TamarinAutoRunner/exampleFiles/oracle.py";
-        String tamarinBin = "/Users/finn/Documents/Research_Project_Tamarin/tamarin-prover/1.6.1/bin/tamarin-prover";
+        String protocol = args[0];
+        String oracleFile = "";
+        String tamarinBin = "";
+        // String protocol =
+        // "/Users/finn/Documents/Research_Project_Tamarin/TamarinAutoRunner/exampleFiles/KC.spthy";
+        // String oracleFile =
+        // "/Users/finn/Documents/Research_Project_Tamarin/TamarinAutoRunner/exampleFiles/oracle.py";
+        // String tamarinBin =
+        // "/Users/finn/Documents/Research_Project_Tamarin/tamarin-prover/1.6.1/bin/tamarin-prover";
 
         // java -jar ./build/libs/TamarinAutoRunner-1.0-SNAPSHOT.jar
         // ./exampleFiles/Netto.spthy -oracle=./exampleFiles/oracle.py
         // -tamarin=/Users/finn/Documents/Research_Project_Tamarin/tamarin-prover/1.6.1/bin/tamarin-prover
-        // for (int i = 1; i < args.length; i++) {
-        // if (args[i].startsWith("-oracle")) {
-        // oracleFile = args[i].split("=")[1];
-        // }
-        // if (args[i].startsWith("-tamarin")) {
-        // tamarinBin = args[i].split("=")[1];
-        // }
-        // }
+        for (int i = 1; i < args.length; i++) {
+            if (args[i].startsWith("-oracle")) {
+                oracleFile = args[i].split("=")[1];
+            }
+            if (args[i].startsWith("-tamarin")) {
+                tamarinBin = args[i].split("=")[1];
+            }
+        }
 
         TamarinFileReader tfr = new TamarinFileReader();
         ArrayList<String> keywords = tfr
